053be6d3737c8ad2bd32f8445e4b9a8cbaa7c0bb
Ayushman <ayushmanchoudhary43@gmail.com>
2026-06-09 21:22:29 +0530

    import changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
-import Homepage from './components/Homepage.jsx'
+import Homepage from './Components/Homepage.jsx'
 import './App.css'
 
 function App() {
